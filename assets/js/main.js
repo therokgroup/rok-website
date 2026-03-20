642c586f32da3f1b2cfd93245f0e05d20cfeabe5
therokgroup <therokgroup@icloud.com>
2026-03-20 13:12:54 +0000

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-
-const toggle = document.querySelector('.menu-toggle');
-const panel = document.querySelector('.mobile-panel');
-if (toggle && panel) {
-  toggle.addEventListener('click', () => {
-    const open = panel.classList.toggle('open');
-    toggle.setAttribute('aria-expanded', open ? 'true' : 'false');
-  });
-}
